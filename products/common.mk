497d38d4e867592cdb2aaa40d66ba9e212745242
kejar31 <kejar31@kejar31-VM-BB>
2012-01-04 10:38:49 -0600

    worked on the props

--- products/common.mk
+++ products/common.mk
@@ -102,7 +102,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.url.legal=http://www.google.com/intl/%s/mobile/android/basic/phone-legal.html \
     ro.url.legal.android_privacy=http://www.google.com/intl/%s/mobile/android/basic/privacy.html \
-    ro.com.android.dataroaming=false \
     ro.com.android.wifi-watchlist=GoogleGuest \
     ro.error.receiver.system.apps=com.google.android.feedback \
     ro.setupwizard.enterprise_mode=1 \
--- products/gummy_toro.mk
+++ products/gummy_toro.mk
@@ -13,12 +13,15 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.com.google.clientidbase=android-verizon \
     ro.com.google.locationfeatures=1 \
     ro.setupwizard.mode=OPTIONAL \
-    ro.cdma.home.operator.numeric=310004 \
-    ro.cdma.home.operator.alpha=Verizon \
     ro.cdma.homesystem=64,65,76,77,78,79,80,81,82,83 \
     ro.cdma.data_retry_config=default_randomization=2000,0,0,120000,180000,540000,960000 \
+    ro.gsm.data_retry_config=max_retries=infinite,default_randomization=2000,0,0,80000,125000,485000,905000 \
     ro.gsm.2nd_data_retry_config=max_retries=infinite,default_randomization=2000,0,0,80000,125000,485000,905000 \
-    ro.cdma.otaspnumschema=SELC,1,80,99
+    ro.cdma.otaspnumschema=SELC,1,80,99 \
+    ro.com.android.dataroaming=false \
+    drm.service.enabled=true \
+    camera.flash_off=0 \
+    dalvik.vm.lockprof.threshold=500 
 
 PRODUCT_COPY_FILES += \
     vendor/samsung/toro/proprietary/com.vzw.hardware.ehrpd.xml:system/etc/permissions/com.vzw.hardware.ehrpd.xml \
