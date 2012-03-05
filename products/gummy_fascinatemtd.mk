fcadaf249e22a506cc12bed2e909daa2ad85e7c1
Kejar31 <justin.a.rogers@gmail.com>
2012-03-04 22:32:09 -0600

    added needed info for fasci build

--- products/gummy_fascinatemtd.mk
+++ products/gummy_fascinatemtd.mk
@@ -3,27 +3,27 @@ $(call inherit-product, vendor/Gummy/products/common.mk)
 $(call inherit-product, vendor/Gummy/products/common_phone.mk)
 
 PRODUCT_NAME := Gummy_fascinatemtd
-PRODUCT_BRAND := google
+PRODUCT_BRAND := samsung
 PRODUCT_DEVICE := fascinatemtd
-PRODUCT_MODEL := Fascinate
+PRODUCT_MODEL := SCH-I500
 PRODUCT_MANUFACTURER := Samsung
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_NAME=sojus \
     BUILD_ID=IML74K \
-    BUILD_DISPLAY_ID=Gummy-CRESPO4G \
-    BUILD_FINGERPRINT="google/sojus/crespo4g:2.3.7/GWK74/185293:user/release-keys" \
-    PRIVATE_BUILD_DESC="sojus-user 2.3.7 GWK74 185293 release-keys"
+    BUILD_DISPLAY_ID=Gummy-FASCINATEMTD \
+    PRODUCT_NAME=SCH-I500 \
+    TARGET_DEVICE=SCH-I500 \
+    BUILD_ID=IML74K BUILD_FINGERPRINT="google/soju/crespo:2.3.4/GRJ22/121341:user/release-keys" \
+    PRIVATE_BUILD_DESC="soju-user 2.3.4 GRJ22 121341 release-keys"
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.goo.developerid=TeamGummy \
-    ro.goo.rom=Gummy-CRESPO4G \
+    ro.goo.rom=Gummy-FASCINATEMTD \
     ro.goo.version=0.8.0 
 
 PRODUCT_COPY_FILES += \
     vendor/Gummy/prebuilt/crespo/bootanimation.zip:system/media/bootanimation.zip
 
-PRODUCT_PACKAGE_OVERLAYS += vendor/Gummy/overlay/crespo4g
 PRODUCT_PACKAGE_OVERLAYS += vendor/Gummy/overlay/common
 PRODUCT_PACKAGE_OVERLAYS += vendor/Gummy/overlay/gsm
 PRODUCT_PACKAGE_OVERLAYS += vendor/Gummy/overlay/no_led
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,7 +2,5 @@ add_lunch_combo Gummy_toro-userdebug
 add_lunch_combo Gummy_maguro-userdebug
 add_lunch_combo Gummy_crespo-userdebug
 add_lunch_combo Gummy_crespo4g-userdebug
-add_lunch_combo Gummy_captivatemtd-userdebug
 add_lunch_combo Gummy_fascinatemtd-userdebug
-add_lunch_combo Gummy_galaxysmtd-userdebug
 add_lunch_combo Gummy_wingray-userdebug
