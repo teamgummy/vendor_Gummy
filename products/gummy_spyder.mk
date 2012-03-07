1a92a5d9964268b0e3ff13fbf949a54bdf100175
Kejar31 <justin.a.rogers@gmail.com>
2012-03-06 21:21:39 -0600

    fix the razr mk

--- products/gummy_spyder.mk
+++ products/gummy_spyder.mk
@@ -1,4 +1,4 @@
-$(call inherit-product, device/motorola/targa/device_spyder.mk)
+$(call inherit-product, device/motorola/spyder/device_spyder.mk)
 $(call inherit-product, vendor/Gummy/products/common.mk)
 $(call inherit-product, vendor/Gummy/products/common_phone.mk)
 
