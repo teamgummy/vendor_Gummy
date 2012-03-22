fb4ca619b8417aa2e03a9ddeed08d2eb7590f43d
Kejar31 <justin.a.rogers@gmail.com>
2012-03-21 19:55:41 -0500

    added D3 and D4

--- products/gummy_maserati.mk
+++ products/gummy_maserati.mk
@@ -1,30 +1,30 @@
-$(call inherit-product, device/motorola/spyder/device_spyder.mk)
+$(call inherit-product, device/motorola/maserati/device_maserati.mk)
 $(call inherit-product, vendor/Gummy/products/common.mk)
 $(call inherit-product, vendor/Gummy/products/common_phone.mk)
 
-PRODUCT_NAME := Gummy_spyder
+PRODUCT_NAME := Gummy_maserati
 PRODUCT_BRAND := google
-PRODUCT_DEVICE := spyder
-PRODUCT_MODEL := DROIDRAZR
+PRODUCT_DEVICE := maserati
+PRODUCT_MODEL := DROID4 
 PRODUCT_MANUFACTURER := Motorola
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_NAME=spyder_vzw \
+    PRODUCT_NAME=maserati_vzw \
     BUILD_ID=IML74K \
-    BUILD_DISPLAY_ID=Gummy-SPYDER \
+    BUILD_DISPLAY_ID=Gummy-MASERATIE \
     BUILD_FINGERPRINT="google/soju/crespo:4.0.3/IML74K/239410:user/release-keys" \
     PRIVATE_BUILD_DESC="soju-user 4.0.3 IML74K 239410 release-keys"
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.goo.developerid=TeamGummy \
-    ro.goo.rom=Gummy-SPYDER \
+    ro.goo.rom=Gummy-MASERATIE \
     ro.goo.version=0.9.0
 
 PRODUCT_COPY_FILES += \
     vendor/Gummy/prebuilt/crespo/bootanimation.zip:system/media/bootanimation.zip
 
 PRODUCT_PACKAGE_OVERLAYS += vendor/Gummy/overlay/common_qHD
-PRODUCT_PACKAGE_OVERLAYS += vendor/Gummy/overlay/spyder
+PRODUCT_PACKAGE_OVERLAYS += vendor/Gummy/overlay/maserati
 PRODUCT_PACKAGE_OVERLAYS += vendor/Gummy/overlay/lte
 
 
