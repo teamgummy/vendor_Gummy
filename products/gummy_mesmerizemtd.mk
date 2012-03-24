e80bb7f3e8ea928f4f7a79ab43e4803542531b6c
Steve Spear <stephen.k.spear@gmail.com>
2012-03-24 11:22:28 -0600

    add .mk files for showcase and mesmerize

---
+++ products/gummy_mesmerizemtd.mk
@@ -0,0 +1,30 @@
+$(call inherit-product, device/samsung/mesmerizemtd/full_mesmerizemtd.mk)
+$(call inherit-product, vendor/Gummy/products/common.mk)
+$(call inherit-product, vendor/Gummy/products/common_phone.mk)
+
+PRODUCT_NAME := Gummy_mesmerizemtd
+PRODUCT_BRAND := samsung
+PRODUCT_DEVICE := mesmerizemtd
+PRODUCT_MODEL := SCH-I500
+PRODUCT_MANUFACTURER := Samsung
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BUILD_ID=IML74K \
+    BUILD_DISPLAY_ID=Gummy-MESMERIZEMTD \
+    PRODUCT_NAME=SCH-I500 \
+    TARGET_DEVICE=SCH-I500 \
+    BUILD_ID=IML74K BUILD_FINGERPRINT="google/soju/crespo:2.3.4/GRJ22/121341:user/release-keys" \
+    PRIVATE_BUILD_DESC="soju-user 2.3.4 GRJ22 121341 release-keys"
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.goo.developerid=TeamGummy \
+    ro.goo.rom=Gummy-MESMERIZEMTD \
+    ro.goo.version=0.9.0 
+
+PRODUCT_COPY_FILES += \
+    vendor/Gummy/prebuilt/crespo/bootanimation.zip:system/media/bootanimation.zip
+
+PRODUCT_PACKAGE_OVERLAYS += vendor/Gummy/overlay/common
+PRODUCT_PACKAGE_OVERLAYS += vendor/Gummy/overlay/mesmerizemtd
+PRODUCT_PACKAGE_OVERLAYS += vendor/Gummy/overlay/no_led
+
