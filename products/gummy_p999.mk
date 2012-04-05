e0c78488914712b7e6f3145e87c40cdda657cde8
kejar31 <justin.a.rogers@gmail.com>
2012-04-05 10:48:41 -0500

    added the G2X device

---
+++ products/gummy_p999.mk
@@ -0,0 +1,30 @@
+$(call inherit-product, device/lge/p999/p999.mk)
+$(call inherit-product, vendor/Gummy/products/common.mk)
+$(call inherit-product, vendor/Gummy/products/common_phone.mk)
+
+PRODUCT_NAME := Gummy_p999
+PRODUCT_DEVICE := p999
+PRODUCT_MODEL := LG-P999 
+PRODUCT_MANUFACTURER := LG
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=soju \
+    BUILD_ID=IMM76D \
+    BUILD_DISPLAY_ID=Gummy-P999 \
+    BUILD_FINGERPRINT="google/soju/crespo:4.0.3/IML74K/239410:user/release-keys" \
+    PRIVATE_BUILD_DESC="soju-user 4.0.3 IML74K 239410 release-keys"
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.goo.developerid=TeamGummy \
+    ro.goo.rom=Gummy-P999 \
+    ro.goo.version=0.9.5 
+
+
+PRODUCT_COPY_FILES += \
+    vendor/Gummy/prebuilt/crespo/bootanimation.zip:system/media/bootanimation.zip
+
+PRODUCT_PACKAGE_OVERLAYS += vendor/Gummy/overlay/p999
+PRODUCT_PACKAGE_OVERLAYS += vendor/Gummy/overlay/common
+PRODUCT_PACKAGE_OVERLAYS += vendor/Gummy/overlay/no_led
+
+
