3ef7141de48464c54ff7d10ef36bd3f2d0cdae46
Dustin Einglett <x13thangelx@gmail.com>
2012-04-16 00:14:26 -0500

    add venus2 (Droid Pro)

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -6,5 +6,6 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/gummy_shadow.mk \
     $(LOCAL_DIR)/gummy_droid2.mk \
     $(LOCAL_DIR)/gummy_droid2we.mk \
+    $(LOCAL_DIR)/gummy_venus2.mk \
     $(LOCAL_DIR)/gummy_p999.mk	
 
---
+++ products/gummy_venus2.mk
@@ -0,0 +1,27 @@
+$(call inherit-product, device/motorola/venus2/venus2.mk)
+$(call inherit-product, vendor/Gummy/products/common.mk)
+$(call inherit-product, vendor/Gummy/products/common_phone.mk)
+
+PRODUCT_NAME := Gummy_venus2
+PRODUCT_BRAND := google
+PRODUCT_DEVICE := venus2
+PRODUCT_MODEL := DROID Pro
+PRODUCT_MANUFACTURER := Motorola
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=venus2_vzw \
+    BUILD_ID=IMM76D \
+    BUILD_DISPLAY_ID=Gummy-venus2 \
+    BUILD_FINGERPRINT="google/soju/crespo:4.0.3/IML74K/239410:user/release-keys" \
+    PRIVATE_BUILD_DESC="soju-user 4.0.3 IML74K 239410 release-keys"
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.goo.developerid=TeamGummy \
+    ro.goo.rom=Gummy-venus2 
+
+PRODUCT_COPY_FILES += \
+    vendor/Gummy/prebuilt/Non-QHD/bootanimation.zip:system/media/bootanimation.zip
+
+PRODUCT_PACKAGE_OVERLAYS += vendor/Gummy/overlay/common
+PRODUCT_PACKAGE_OVERLAYS += vendor/Gummy/overlay/venus2
+PRODUCT_PACKAGE_OVERLAYS += vendor/Gummy/overlay/no_led
