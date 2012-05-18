96a63da73e01cf7599095104bae3bc733931e010
kejar31@kejar31 <jenkins@cyanogenmod.com>
2012-05-17 19:56:51 -0500

    updated version number for goo

--- products/common.mk
+++ products/common.mk
@@ -141,7 +141,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.kernel.android.checkjni=0 \
     windowsmgr.max_events_per_sec=240 \
     wifi.supplicant_scan_interval=180 \
-    ro.goo.version=120
+    ro.goo.version=123
 
 
 
