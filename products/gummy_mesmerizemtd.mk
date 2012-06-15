8bf5b7515f57a00e32e1b9ce072a058ae88b44e8
Steve Spear <stephen.k.spear@gmail.com>
2012-06-14 18:46:13 -0600

    Forgot about us.

--- products/gummy_mesmerizemtd.mk
+++ products/gummy_mesmerizemtd.mk
@@ -1,7 +1,6 @@
 $(call inherit-product, device/samsung/mesmerizemtd/full_mesmerizemtd.mk)
 $(call inherit-product, vendor/Gummy/products/common.mk)
 $(call inherit-product, vendor/Gummy/products/common_phone.mk)
-$(call inherit-product, vendor/Gummy/products/gummy_themes.mk)
 
 PRODUCT_NAME := Gummy_mesmerizemtd
 PRODUCT_BRAND := samsung
